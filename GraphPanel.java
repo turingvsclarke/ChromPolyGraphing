6e3f14eb28ada3529c8068f10c5fc380b5292afc
Eades <ryeades@134-68-176-156.dhcp-in.iupui.edu>
2021-04-25 12:21:29 -0400

    old version of GraphPanel.java

--- GraphPanel.java
+++ GraphPanel.java
@@ -1,47 +1,52 @@
 // GraphPanel.java
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
+
 public class GraphPanel extends JPanel{
     int height;
     int width;
     GraphNormal graph;
     public static void main(String[] args) {
-
     }
 
     public GraphPanel(){
-	// create a random graph with 10 vertices and 3 edges
-	this.graph = new GraphNormal(100,100);
+	// create a random graph with 3 vertices and 3 edges
+	this.graph = new GraphNormal(3,3);
 	this.height = this.getHeight();
 	this.width = this.getWidth();
 	this.setLayout(null);
 	this.paintVertices();
+	this.graph.printVertices();
     } // end constructor
 
     public void paintVertices(){
 	// Add all the vertices to the graph
 	GraphNormal g = this.graph;
+	ArrayList buttons = new ArrayList();
 	int v = g.getVertices().size();
-	for(int i=0;i<1;i++){	
-		VertexButton b = new VertexButton();
+	for(int i=0;i<v;i++){	
 		Vertex currentV = g.getVertex(i);
 		int vX = currentV.getX();
 		int vY = currentV.getY();
+
+		JButton b = new JButton("Vertex "+i);
+		buttons.add(b);
 		int w = b.getWidth();
 		int h = b.getHeight();
 		// Have the button be centered at the vertex's coordinates
-		b.setBounds(500,500,100,100);
+		b.setBounds(vX,vY,w,h);
 		this.add(b);
 	} // end for
 
     } // end paintVertices
-
-	
+    /***	
     public void paintComponent(Graphics g){
 	// paint the graph
 
 	// Draw all the edges
 	for(int i=0;i<graph.getEdges().size();i++){
+	
 		Edge edge = graph.getEdge(i);
 		Vertex V1 = edge.getV1();
 		Vertex V2 = edge.getV2();
@@ -58,5 +63,5 @@ public class GraphPanel extends JPanel{
 	} // end for
 	// For erasing use g.setColor(this.getBackgroundColor).
     } // end paintComponent
-
+    ***/
 } // end GraphPanel
